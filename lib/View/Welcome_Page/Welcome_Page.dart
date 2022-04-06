ab9f46bfefd907e9513edf9a0598ea5d60939334
Kevin Patoiya <Kevinbpatoliya23@gmail.com>
2022-04-06 15:40:47 +0530

    welcome screen background done,Text added on welcome screen

--- lib/View/Welcome_Page/Welcome_Page.dart
+++ lib/View/Welcome_Page/Welcome_Page.dart
@@ -1,15 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
+import 'components/background.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      /*appBar: AppBar(
+      /* appBar: AppBar(
         title: const Text("WelcomePage"),
       ),*/
-      body: const Center(child: Text("Welcome to code Algo")),
+      body: Background(
+        size: size,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Welcome to Code Algo Family",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/View/Welcome_Page/components/background.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.size,
+    required this.child,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/main_top.png",
+              width: size.width * 0.4,
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: Image.asset(
+              "assets/images/bottom.png",
+              width: size.width * 0.4,
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
